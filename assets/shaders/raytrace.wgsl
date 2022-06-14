2599937b01af1bb9b7bb33a69f860f311591cb5b
Patrick Towles <james.towles@gmail.com>
2022-06-14 04:07:08 -0400

    have working bindgroups though i recreate them every frame, getting there

--- assets/shaders/raytrace.wgsl
+++ assets/shaders/raytrace.wgsl
@@ -1,77 +1,102 @@
+struct Settings {
+    size: vec2<u32>;
+};
+
+struct Camera {
+    position: vec3<f32>;
+    p0: vec3<f32>;
+    p1: vec3<f32>;
+    p2: vec3<f32>;
+};
+
+struct Intersection {
+    t: f32;                    // intersection distance along ray
+    u: f32;
+    v: f32;	                // barycentric coordinates of the intersection
+    inst_prim: u32;              // instance index (12 bit) and primitive index (20 bit)
+};
+ 
+struct Ray {
+    origin: vec3<f32>;
+    direction: vec3<f32>;
+    inv_direction: vec3<f32>;
+    hit: Intersection;  
+};
+
 [[group(0), binding(0)]]
 var texture: texture_storage_2d<rgba8unorm, read_write>;
 
-// struct Settings {
-//     size: vec2<u32>,
-// };
-// [[group(2), binding(0)]]
-// var<uniform> settings: Settings;
+[[group(0), binding(1)]]
+var<uniform> settings: Settings;
 
-
-fn hash(value: u32) -> u32 {
-    var state = value;
-    state = state ^ 2747636419u;
-    state = state * 2654435769u;
-    state = state ^ state >> 16u;
-    state = state * 2654435769u;
-    state = state ^ state >> 16u;
-    state = state * 2654435769u;
-    return state;
-}
-
-fn randomFloat(value: u32) -> f32 {
-    return f32(hash(value)) / 4294967295.0;
-}
+// fn circle(st: vec2<f32>, radius: f32) -> f32 {
+//     var dist = st - vec2<f32>(0.5);
+// 	return  1.0 - smoothStep(radius - (radius * 0.01),
+//                          radius + (radius * 0.01),
+//                          dot(dist, dist) * 4.0);
+// }
 
 [[stage(compute), workgroup_size(8, 8, 1)]]
 fn init([[builtin(global_invocation_id)]] invocation_id: vec3<u32>, [[builtin(num_workgroups)]] num_workgroups: vec3<u32>) {
     let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
     let location_f32 = vec2<f32>(f32(invocation_id.x), f32(invocation_id.y));
+    let uv = vec2<f32>(location_f32.x / f32(settings.size.x), location_f32.y / f32(settings.size.y));
 
+    //let c = circle(uv, 0.5);
+
+    let color = vec4<f32>(0.0, 0.0, 0.0, 1.0);
     // let randomNumber = randomFloat(invocation_id.y * num_workgroups.x + invocation_id.x);
     // let alive = randomNumber > 0.9;
     // let color = vec4<f32>(f32(alive));
 
     // plot a pixel into the target array in GPU memory
-    let color = vec4<f32>(clamp(0.0, 1.0, location_f32.x / 1024.0), 0.0, 0.0, 1.0);
+
+
+    storageBarrier();
     textureStore(texture, location, color);
-}
-
-
-fn get(location: vec2<i32>, offset_x: i32, offset_y: i32) -> i32 {
-    let value: vec4<f32> = textureLoad(texture, location + vec2<i32>(offset_x, offset_y));
-    return i32(value.x);
-}
-
-fn count_alive(location: vec2<i32>) -> i32 {
-    return get(location, -1, -1) +
-           get(location, -1,  0) +
-           get(location, -1,  1) +
-           get(location,  0, -1) +
-           get(location,  0,  1) +
-           get(location,  1, -1) +
-           get(location,  1,  0) +
-           get(location,  1,  1);
 }
 
 [[stage(compute), workgroup_size(8, 8, 1)]]
 fn update([[builtin(global_invocation_id)]] invocation_id: vec3<u32>) {
-    // let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
-
-    // let n_alive = count_alive(location);
-    // let color = vec4<f32>(f32(n_alive) / 8.0);
-
-    // var alive: bool;
-    // if (n_alive == 3) {
-    //     alive = true;
-    // } else if (n_alive == 2) {
-    //     let currently_alive = get(location, 0, 0);
-    //     alive = bool(currently_alive);
-    // } else {
-    //     alive = false;
-    // }
-
-    // storageBarrier();
-
-    // textureStore(texture, location, vec4<f32>(f32(alive)));
+    let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
+    let location_f32 = vec2<f32>(f32(invocation_id.x), f32(invocation_id.y));
+    let uv = vec2<f32>(location_f32.x / f32(settings.size.x), location_f32.y / f32(settings.size.y));
+    // plot a pixel into the target array in GPU memory
+    let color = vec4<f32>(uv.x, uv.y, 0.0, 1.0);
+    
+    storageBarrier();
+    textureStore(texture, location, color);
 }
+
+
+
+// fn hash(value: u32) -> u32 {
+//     var state = value;
+//     state = state ^ 2747636419u;
+//     state = state * 2654435769u;
+//     state = state ^ state >> 16u;
+//     state = state * 2654435769u;
+//     state = state ^ state >> 16u;
+//     state = state * 2654435769u;
+//     return state;
+// }
+
+// fn randomFloat(value: u32) -> f32 {
+//     return f32(hash(value)) / 4294967295.0;
+// }
+
+// fn get(location: vec2<i32>, offset_x: i32, offset_y: i32) -> i32 {
+//     let value: vec4<f32> = textureLoad(texture, location + vec2<i32>(offset_x, offset_y));
+//     return i32(value.x);
+// }
+
+// fn count_alive(location: vec2<i32>) -> i32 {
+//     return get(location, -1, -1) +
+//            get(location, -1,  0) +
+//            get(location, -1,  1) +
+//            get(location,  0, -1) +
+//            get(location,  0,  1) +
+//            get(location,  1, -1) +
+//            get(location,  1,  0) +
+//            get(location,  1,  1);
+// }
--- examples/raytrace.rs
+++ examples/raytrace.rs
@@ -8,22 +8,16 @@ use bevy::{
         extract_resource::{ExtractResource, ExtractResourcePlugin},
         render_asset::RenderAssets,
         render_graph::{self, RenderGraph},
-        render_resource::{
-            BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout,
-            BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingResource, BindingType,
-            CachedComputePipelineId, CachedPipelineState, ComputePassDescriptor,
-            ComputePipelineDescriptor, Extent3d, PipelineCache, ShaderStages, StorageTextureAccess,
-            TextureDimension, TextureFormat, TextureUsages, TextureViewDimension, BufferBindingType, BufferSize, ShaderType,
-        },
-        renderer::{RenderContext, RenderDevice},
+        render_resource::*,
+        renderer::{RenderContext, RenderDevice, RenderQueue},
+        view::window,
         RenderApp, RenderStage,
     },
-    window::PresentMode,
+    window::{PresentMode, WindowResized},
 };
 use bevy_slyedoc_bvh::prelude::*;
 use helpers::*;
 
-const SIZE: (u32, u32) = (1280, 720);
 const WORKGROUP_SIZE: u32 = 8;
 
 fn main() {
@@ -35,41 +29,50 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(HelperPlugin) // See cusor plugin in helper plugins
         .add_plugin(BvhPlugin)
-        //.add_plugin(DebugLinesPlugin::default())
+        .add_plugin(RaytracePlugin)
         //.add_startup_system(helpers::setup_cameras)
         .add_startup_system(helpers::load_enviroment)
         //.add_startup_system(helpers::load_sponza)
-        .add_plugin(RaytracePlugin)
         .add_startup_system(setup)
+        .add_system(resize_sprite)
         .run();
 }
 
-fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
-    let mut image = Image::new_fill(
-        Extent3d {
-            width: SIZE.0,
-            height: SIZE.1,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &[0, 0, 0, 255],
-        TextureFormat::Rgba8Unorm,
-    );
-    image.texture_descriptor.usage =
-        TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
-    let image = images.add(image);
+// Marker for Sprite for resize
+#[derive(Component)]
+struct RtSprite;
 
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(SIZE.0 as f32, SIZE.1 as f32)),
+fn setup(mut commands: Commands, mut image: Res<RtImage>, window: Res<WindowDescriptor>) {
+    let size = UVec2::new(window.width as u32, window.height as u32);
+
+    // create the image
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(size.x as f32, size.y as f32)),
+                ..default()
+            },
+            texture: image.0.clone(),
             ..default()
-        },
-        texture: image.clone(),
-        ..default()
-    });
-    commands.spawn_bundle(Camera2dBundle::default());
+        })
+        .insert(RtSprite);
 
-    commands.insert_resource(RaytraceImage(image));
+    commands.spawn_bundle(Camera2dBundle::default());
+}
+
+fn resize_sprite(
+    mut commands: Commands,
+    mut images: ResMut<Assets<Image>>,
+    mut sprite_query: Query<(&mut Sprite), With<RtSprite>>,
+    mut rt_image: ResMut<RtImage>,
+    mut resize_event: EventReader<WindowResized>,
+) {
+    for resize in resize_event.iter() {
+        info!("resize {:?}", resize);
+        let mut sprite = sprite_query.single_mut();
+
+        sprite.custom_size = Some(Vec2::new(resize.width, resize.height));
+    }
 }
 
 pub struct RaytracePlugin;
@@ -78,143 +81,248 @@ impl Plugin for RaytracePlugin {
     fn build(&self, app: &mut App) {
         // Extract the raytrace image resource from the main world into the render world
         // for operation on by the compute shader and display on the sprite.
-        app.add_plugin(ExtractResourcePlugin::<RaytraceImage>::default());
+        app.init_resource::<RtSettings>() 
+            .init_resource::<RtImage>()                       
+            .add_plugin(ExtractResourcePlugin::<RtImage>::default())
+            .add_plugin(ExtractResourcePlugin::<RtSettings>::default())
+            .add_system(Self::resize_window);
+
+        let render_device = app.world.resource::<RenderDevice>();
+
         let render_app = app.sub_app_mut(RenderApp);
         render_app
-            .init_resource::<RaytracePipeline>()
-            .add_system_to_stage(RenderStage::Queue, queue_bind_group);
+            .init_resource::<RtPipeline>()
+            .add_system_to_stage(RenderStage::Queue, Self::queue_bind_group);
 
         let mut render_graph = render_app.world.resource_mut::<RenderGraph>();
-        render_graph.add_node("raytrace", RaytraceNode::default());
+        render_graph.add_node("raytrace", RtNode::default());
         render_graph
             .add_node_edge("raytrace", bevy::render::main_graph::node::CAMERA_DRIVER)
             .unwrap();
     }
 }
 
-#[derive(ShaderType)]
-struct RaytraceSettings {
+impl RaytracePlugin {
+
+    fn resize_window(
+        mut rt_settings: ResMut<RtSettings>,
+        mut rt_image: ResMut<RtImage>,
+        mut images: ResMut<Assets<Image>>,
+        mut resize_event: EventReader<WindowResized>,
+    ) {
+        for resize in resize_event.iter() {
+            rt_settings.size.x = resize.width as u32;
+            rt_settings.size.y = resize.height as u32;
+
+            // TODO: This doesnt work, handle gets updated correctly, but image isnt displayed
+            // Has something to do with the binding
+            // let mut image = Image::new_fill(
+            //     Extent3d {
+            //         width: rt_settings.size.x,
+            //         height: rt_settings.size.y,
+            //         depth_or_array_layers: 1,
+            //     },
+            //     TextureDimension::D2,
+            //     &[0, 0, 0, 255],
+            //     TextureFormat::Rgba8Unorm,
+            // );
+            // image.texture_descriptor.usage = TextureUsages::COPY_DST
+            //     | TextureUsages::STORAGE_BINDING
+            //     | TextureUsages::TEXTURE_BINDING;
+
+            // rt_image.0 = images.add(image);
+        }
+    }
+
+    fn queue_bind_group(
+        mut commands: Commands,
+        settings: Res<RtSettings>,
+        pipeline: Res<RtPipeline>,
+        gpu_images: Res<RenderAssets<Image>>,
+        raytrace_image: Res<RtImage>,
+        render_device: Res<RenderDevice>,
+    ) {
+         // image
+        let view = &gpu_images[&raytrace_image.0];
+
+        // settings
+        let byte_buffer = [0u8; RtSettings::SIZE.get() as usize];
+        let mut settings_buffer = encase::UniformBuffer::new(byte_buffer);
+        settings_buffer.write(&settings.into_inner()).unwrap();
+
+        let settings_buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
+            label: Some("rt settings uniform buffer"),
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+            contents: settings_buffer.as_ref(),
+        });
+
+        let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
+            label: None,
+            layout: &pipeline.bind_group_layout,
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: BindingResource::TextureView(&view.texture_view),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: settings_buffer.as_entire_binding(),
+                },
+            ],
+        });
+        commands.insert_resource(RtSettingsGpu {
+            settings_buffer,
+            bind_group,
+        });
+    }
+}
+
+#[derive(ShaderType, Debug, Clone, ExtractResource)]
+struct RtSettings {
     pub size: UVec2,
 }
 
+impl FromWorld for RtSettings {
+    fn from_world(world: &mut World) -> Self {
+        let window = world.resource::<WindowDescriptor>();
+        let size = UVec2::new(window.width as u32, window.height as u32);
+        RtSettings { size }
+    }
+}
 #[derive(Clone, Deref, ExtractResource)]
-struct RaytraceImage(Handle<Image>);
+struct RtImage(Handle<Image>);
 
-struct RaytraceBindGroup(BindGroup);
-
-fn queue_bind_group(
-    mut commands: Commands,
-    pipeline: Res<RaytracePipeline>,
-    gpu_images: Res<RenderAssets<Image>>,
-    raytrace_image: Res<RaytraceImage>,
-    render_device: Res<RenderDevice>,
-) {
-    let view = &gpu_images[&raytrace_image.0];
-    let bind_group = render_device.create_bind_group(&BindGroupDescriptor {
-        label: None,
-        layout: &pipeline.texture_bind_group_layout,
-        entries: &[
-            BindGroupEntry {
-                binding: 0,
-                resource: BindingResource::TextureView(&view.texture_view),
+impl FromWorld for RtImage {
+    fn from_world(world: &mut World) -> Self {
+        let settings = world.resource::<RtSettings>();
+        let mut image = Image::new_fill(
+            Extent3d {
+                width: settings.size.x,
+                height: settings.size.y,
+                depth_or_array_layers: 1,
             },
-        ],
-    });
-    commands.insert_resource(RaytraceBindGroup(bind_group));
+            TextureDimension::D2,
+            &[0, 0, 0, 255],
+            TextureFormat::Rgba8Unorm,
+        );
+        image.texture_descriptor.usage = TextureUsages::COPY_DST
+            | TextureUsages::STORAGE_BINDING
+            | TextureUsages::TEXTURE_BINDING;
+        let mut images = world.resource_mut::<Assets<Image>>();
+        let handle = images.add(image);
+        RtImage(handle)
+    }
+}
+struct RtSettingsGpu {
+    settings_buffer: Buffer,
+    bind_group: BindGroup,
 }
 
-pub struct RaytracePipeline {
-    texture_bind_group_layout: BindGroupLayout,
+pub struct RtPipeline {
+    bind_group_layout: BindGroupLayout,
     init_pipeline: CachedComputePipelineId,
     update_pipeline: CachedComputePipelineId,
 }
 
-impl FromWorld for RaytracePipeline {
+impl FromWorld for RtPipeline {
     fn from_world(world: &mut World) -> Self {
-        let texture_bind_group_layout =
+        let bind_group_layout =
             world
                 .resource::<RenderDevice>()
                 .create_bind_group_layout(&BindGroupLayoutDescriptor {
-                    label: None,
-                    entries: &[BindGroupLayoutEntry {
-                        binding: 0,
-                        visibility: ShaderStages::COMPUTE,
-                        ty: BindingType::StorageTexture {
-                            access: StorageTextureAccess::ReadWrite,
-                            format: TextureFormat::Rgba8Unorm,
-                            view_dimension: TextureViewDimension::D2,
+                    label: Some("rt bind group layout"),
+                    entries: &[
+                        BindGroupLayoutEntry {
+                            binding: 0,
+                            visibility: ShaderStages::COMPUTE,
+                            ty: BindingType::StorageTexture {
+                                access: StorageTextureAccess::ReadWrite,
+                                format: TextureFormat::Rgba8Unorm,
+                                view_dimension: TextureViewDimension::D2,
+                            },
+                            count: None,
                         },
-                        count: None,
-                    },
+                        BindGroupLayoutEntry {
+                            binding: 1,
+                            visibility: ShaderStages::COMPUTE,
+                            ty: BindingType::Buffer {
+                                ty: BufferBindingType::Uniform,
+                                has_dynamic_offset: false,
+                                min_binding_size: Some(RtSettings::min_size()),                                
+                            },
+                            count: None,
+                        },
                     ],
                 });
+
         let shader = world
             .resource::<AssetServer>()
             .load("shaders/raytrace.wgsl");
+
         let mut pipeline_cache = world.resource_mut::<PipelineCache>();
         let init_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
             label: None,
-            layout: Some(vec![texture_bind_group_layout.clone()]),
+            layout: Some(vec![bind_group_layout.clone()]),
             shader: shader.clone(),
             shader_defs: vec![],
             entry_point: Cow::from("init"),
         });
         let update_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
             label: None,
-            layout: Some(vec![texture_bind_group_layout.clone()]),
+            layout: Some(vec![bind_group_layout.clone()]),
             shader,
             shader_defs: vec![],
             entry_point: Cow::from("update"),
         });
 
-        RaytracePipeline {
-            texture_bind_group_layout,
+        RtPipeline {
+            bind_group_layout,
             init_pipeline,
             update_pipeline,
         }
     }
 }
 
-enum RaytraceState {
+enum RtState {
     Loading,
     Init,
     Update,
 }
 
-struct RaytraceNode {
-    state: RaytraceState,
+struct RtNode {
+    state: RtState,
 }
 
-impl Default for RaytraceNode {
+impl Default for RtNode {
     fn default() -> Self {
         Self {
-            state: RaytraceState::Loading,
+            state: RtState::Loading,
         }
     }
 }
 
-impl render_graph::Node for RaytraceNode {
+impl render_graph::Node for RtNode {
     fn update(&mut self, world: &mut World) {
-        let pipeline = world.resource::<RaytracePipeline>();
+        let pipeline = world.resource::<RtPipeline>();
         let pipeline_cache = world.resource::<PipelineCache>();
 
         // if the corresponding pipeline has loaded, transition to the next stage
         match self.state {
-            RaytraceState::Loading => {
+            RtState::Loading => {
                 if let CachedPipelineState::Ok(_) =
                     pipeline_cache.get_compute_pipeline_state(pipeline.init_pipeline)
                 {
-                    self.state = RaytraceState::Init;
+                    self.state = RtState::Init;
                 }
             }
-            RaytraceState::Init => {
+            RtState::Init => {
                 if let CachedPipelineState::Ok(_) =
                     pipeline_cache.get_compute_pipeline_state(pipeline.update_pipeline)
                 {
-                    self.state = RaytraceState::Update;
+                    self.state = RtState::Update;
                 }
             }
-            RaytraceState::Update => {}
+            RtState::Update => {}
         }
     }
 
@@ -224,32 +332,41 @@ impl render_graph::Node for RaytraceNode {
         render_context: &mut RenderContext,
         world: &World,
     ) -> Result<(), render_graph::NodeRunError> {
-        let texture_bind_group = &world.resource::<RaytraceBindGroup>().0;
+        let rt_bind_group = &world.resource::<RtSettingsGpu>().bind_group;
         let pipeline_cache = world.resource::<PipelineCache>();
-        let pipeline = world.resource::<RaytracePipeline>();
+        let pipeline = world.resource::<RtPipeline>();
+        let rt_settings = world.resource::<RtSettings>();
 
         let mut pass = render_context
             .command_encoder
             .begin_compute_pass(&ComputePassDescriptor::default());
 
-        pass.set_bind_group(0, texture_bind_group, &[]);
+        pass.set_bind_group(0, rt_bind_group, &[]);
 
         // select the pipeline based on the current state
         match self.state {
-            RaytraceState::Loading => {}
-            RaytraceState::Init => {
+            RtState::Loading => {}
+            RtState::Init => {
                 let init_pipeline = pipeline_cache
                     .get_compute_pipeline(pipeline.init_pipeline)
                     .unwrap();
                 pass.set_pipeline(init_pipeline);
-                pass.dispatch(SIZE.0 / WORKGROUP_SIZE, SIZE.1 / WORKGROUP_SIZE, 1);
+                pass.dispatch(
+                    rt_settings.size.x / WORKGROUP_SIZE,
+                    rt_settings.size.y / WORKGROUP_SIZE,
+                    1,
+                );
             }
-            RaytraceState::Update => {
+            RtState::Update => {
                 let update_pipeline = pipeline_cache
                     .get_compute_pipeline(pipeline.update_pipeline)
                     .unwrap();
                 pass.set_pipeline(update_pipeline);
-                pass.dispatch(SIZE.0 / WORKGROUP_SIZE, SIZE.1 / WORKGROUP_SIZE, 1);
+                pass.dispatch(
+                    rt_settings.size.x / WORKGROUP_SIZE,
+                    rt_settings.size.y / WORKGROUP_SIZE,
+                    1,
+                );
             }
         }
 
